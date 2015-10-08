dac2ed7dfe8441b40d96c31b05b1830f0c169556
Henning Kage <henning.kage@gmail.com>
2015-10-08 11:55:18 +0200

    Added migration directories

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 from setuptools import setup, find_packages
 
-exec(open('app_name/version.py').read())
-setup(name='app_name',
-      version=__version__,
-      description='',
-      long_description='',
-      author='Henning Kage',
-      author_email='<henning.kage@gmail.com>',
-      url='',
-      include_package_data=True,
-      classifiers=[],
-      packages=find_packages(exclude=['tests']),
-      install_requires=['pytest==2.6.1'])
+exec(open('face_off/version.py').read())
+
+setup(name='django-face-off',
+    version=__version__,
+    description='',
+    long_description='',
+    author='Henning Kage',
+    author_email='<henning.kage@gmail.com>',
+    url='',
+    include_package_data=True,
+    classifiers=[],
+    packages=find_packages(exclude=['tests']),
+    install_requires=['pytest==2.6.1'])
