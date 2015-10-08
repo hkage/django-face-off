9802675bb0702ae8f0abb7cf0b9727193d699527
Henning Kage <henning.kage@gmail.com>
2015-10-08 14:42:28 +0200

    Added example project

---
+++ face_off/middleware.py
@@ -0,0 +1,6 @@
+"""Middleware for django-face-off"""
+
+class UserRepresentationMiddleware(object):
+
+    def process_request(self, request):
+        pass
--- face_off/models.py
+++ face_off/models.py
@@ -1,12 +1,18 @@
-from django.contrib.auth import get_user_model
+
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
+
+try:
+    from django.contrib.auth import get_user_model
+    User = get_user_model()
+except ImportError:
+    from django.contrib.auth.models import User
 
 
 class UserRepresentation(models.Model):
 
-    user = models.ForeignKey(get_user_model())
-    representative = models.ForeignKey(get_user_model())
+    user = models.ForeignKey(User, related_name='represented_users')
+    representative = models.ForeignKey(User, related_name='representatives')
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
